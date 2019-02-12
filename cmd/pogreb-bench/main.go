e7fc49a8861c1b9deeabaeec8653133f3abc0bd3
Artem Krylysov <kadotex@gmail.com>
2019-02-11 22:07:25 -0500

    Add option for taking profile

--- cmd/pogreb-bench/main.go
+++ cmd/pogreb-bench/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	"github.com/pkg/profile"
 )
 
 var (
@@ -16,15 +18,28 @@ var (
 	concurrency  = flag.Int("c", 1, "number of concurrent goroutines")
 	dir          = flag.String("d", "", "database directory")
 	progress     = flag.Bool("p", false, "show progress")
+	profileMode  = flag.String("profile", "", "enable profile. cpu, mem, block or mutex")
 )
 
 func main() {
-	//defer profile.Start(profile.MemProfile, profile.ProfilePath(".")).Stop()
 	flag.Parse()
+
 	if *dir == "" {
 		flag.Usage()
 		return
 	}
+
+	switch *profileMode {
+	case "cpu":
+		defer profile.Start(profile.CPUProfile).Stop()
+	case "mem":
+		defer profile.Start(profile.MemProfile).Stop()
+	case "block":
+		defer profile.Start(profile.BlockProfile).Stop()
+	case "mutex":
+		defer profile.Start(profile.MutexProfile).Stop()
+	}
+
 	if err := benchmark(*engine, *dir, *numKeys, *minKeySize, *maxKeySize, *minValueSize, *maxValueSize, *concurrency, *progress); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
